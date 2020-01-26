d4fcbdac8ecff2e21f6667b256cab18f90747fa4
fox <fox@vvfox.com>
2020-01-27 06:53:16 +0800

    fix: fab style

--- packages/transparent-info-app/pages/event.tsx
+++ packages/transparent-info-app/pages/event.tsx
@@ -1,12 +1,28 @@
 import Layout from '../components/Layout';
 import { TimeLineEvent } from '../components/TimeLine';
-import { Container, Fab } from '@material-ui/core';
+import { Container, Fab, Theme } from '@material-ui/core';
 import React, { useState } from 'react';
 import { SortBy } from '../src/consts';
 import SortIcon from '@material-ui/icons/Sort';
+import { makeStyles, createStyles } from '@material-ui/core/styles';
+import { green } from '@material-ui/core/colors';
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    fab: {
+      position: 'fixed',
+      bottom: theme.spacing(8),
+      right: theme.spacing(4),
+      '&:hover': {
+        backgroundColor: green[600],
+      },
+    },
+  })
+);
 
 export default () => {
   const [sortBy, setSortBy] = useState(SortBy.Desc);
+
+  const classes = useStyles();
 
   const changeSort = (e: React.MouseEvent) => {
     if (sortBy == SortBy.Asc) {
@@ -20,7 +36,7 @@ export default () => {
     <Layout title="新型冠状病毒突发事件发展路线">
       <Container>
         <TimeLineEvent sortBy={sortBy} />
-        <Fab onClick={changeSort}>
+        <Fab onClick={changeSort} className={classes.fab}>
           <SortIcon color={sortBy === SortBy.Asc ? 'secondary' : 'primary'} />
         </Fab>
       </Container>
