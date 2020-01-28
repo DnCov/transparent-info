d4af978d8a5aa6dfc2d15768797ef99766ac1e44
fox <fox@vvfox.com>
2020-01-29 07:52:32 +0800

    hack baseUrl :prerelease

--- packages/transparent-info-app/next.config.js
+++ packages/transparent-info-app/next.config.js
@@ -8,12 +8,18 @@ const withMDX = require('@next/mdx')({
 });
 
 const assetPrefix = process.env['ASSETS_PREFIX'] || '/';
+const basePath = process.env['BASE_PATH'] || '/';
 
 module.exports = withMDX({
   pageExtensions: ['js', 'jsx', 'md', 'mdx', 'tsx'],
   assetPrefix: assetPrefix,
+  basePath: basePath,
+  experimental: {
+    basePath: basePath,
+  },
   env: {
     ASSETS_PREFIX: assetPrefix,
+    BASE_PATH: basePath,
   },
   // assetPrefix: '/ipns/Qmd3HzpnpSsLeUQF2mJEXn24a1yYo2LTaQyoq4mwxkse1Z/',
 });
